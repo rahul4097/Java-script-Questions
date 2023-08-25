f759cc9d5b66a3f7ab879d23df207e3600d63e32
rahul4097 <138388064+rahul4097@users.noreply.github.com>
2023-08-25 12:41:51 +0530

    Update Java_script_question.js

--- Java_script_question.js
+++ Java_script_question.js
@@ -1,4 +1,4 @@
-//print even numbers between 1 and 20 but exclude multiole of 6.
+// Q 1) Print even numbers between 1 and 20 but exclude multiole of 6.
 
 // const even_numbers=(num)=>{
 //     for(let i=1;i<num;i++){
@@ -9,15 +9,13 @@
 //         }
 //     }
 // }
-
 // even_numbers(20);
 
-//Calculate the total price of items in a shopping cart, but apply a 10% discount to items that are on sale.
+// Q 2) Calculate the total price of items in a shopping cart, but apply a 10% discount to items that are on sale.
 
 // const shoppingCart= [ {items: 'Shirt', price:25,onSale:false},{items: 'Jeans',price:50, onSale:true},{items: 'Shoes',price:80, onSale:false},{items: 'Hat', price:15, onSale:true}];
 // var sum=0;
 // for(let x of shoppingCart){
-    
 //     // console.log(x.price);
 //     let prices=x.price;
 //     if(x.onSale==true){
@@ -28,7 +26,9 @@
 // }
 // console.log(sum);
 
-//Remove the odd numbers form an array and get only the even numbers in a new array using filter function.
+
+// Q3) Remove the odd numbers form an array and get only the even numbers in a new array using filter function.
+
 // const arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 // const remove_odd_num= arr.filter( (num) =>  {
 //     if(num%2==0){
@@ -37,7 +37,8 @@
 //     console.log(num);
 // });
 
-//Double each number in an array using the map function.
+
+// Q 4) Double each number in an array using the map function.
 
 // const arr=[10,20,30,40,50];
 // arr.map((num) => {
@@ -45,16 +46,17 @@
 //     console.log(double);
 // })
 
-// Calculate the sum of the numbres in an array using the reduce function.
+
+// Q 5) Calculate the sum of the numbres in an array using the reduce function.
 
 // const arr=[100,200,300,400,500];
-
 // const total=arr.reduce( (x,y) =>{
 //     return x + y ;
 // })
 // console.log(total);
 
-// Find the first even number in an array using find function.
+
+// Q 6) Find the first even number in an array using find function.
 
 // const arr=[13,8,3,4,5,6,7,10,9];
 // const even= arr.find((num) =>{
